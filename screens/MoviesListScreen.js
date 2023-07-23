6d17224c9c7448cfeb190be125edd01d65c1e4a3
Hasham Asad <tipuasad2001@gamil.com>
2023-07-23 15:03:28 +0500

    no stylings

---
+++ screens/MoviesListScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FlatList } from "react-native";
+import Row from "../components/Row";
+
+const renderItem = obj => <Row {...obj.item} />
+
+const MoviesListScreen = ({route}) => {
+
+    const movies = route.params.Movies.Search;
+    return (
+        <FlatList
+        renderItem={renderItem}
+        data={movies} 
+        /> 
+    )
+    }
+
+
+export default MoviesListScreen
